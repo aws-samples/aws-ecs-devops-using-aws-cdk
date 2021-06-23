fdb33e527daab1d38e323c094d2cd9924e057cad
Kwonyul <kwonyul@amazon.com>
2021-06-24 04:03:48 +0900

    Update setup script

--- scripts/setup_initial.sh
+++ scripts/setup_initial.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Configuration File Path
-CONFIG_INFRA=$1
+APP_CONFIG=$1
+export APP_CONFIG=$1
 
 echo ==--------CheckDedendencies---------==
 # npm install -g aws-cdk
@@ -10,12 +11,12 @@ npm --version
 cdk --version
 jq --version
 
-ACCOUNT=$(cat $CONFIG_INFRA | jq -r '.Project.Account') #ex> 123456789123
-REGION=$(cat $CONFIG_INFRA | jq -r '.Project.Region') #ex> us-east-1
-PROFILE_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Profile') #ex> cdk-demo
+ACCOUNT=$(cat $APP_CONFIG | jq -r '.Project.Account') #ex> 123456789123
+REGION=$(cat $APP_CONFIG | jq -r '.Project.Region') #ex> us-east-1
+PROFILE_NAME=$(cat $APP_CONFIG | jq -r '.Project.Profile') #ex> cdk-demo
 
 echo ==--------ConfigInfo---------==
-echo $CONFIG_INFRA
+echo $APP_CONFIG
 echo $ACCOUNT
 echo $REGION
 echo $PROFILE_NAME
@@ -29,6 +30,7 @@ echo .
 
 echo ==--------BootstrapCDKEnvironment---------==
 cdk bootstrap aws://$ACCOUNT/$REGION --profile $PROFILE_NAME
+# cdk bootstrap aws://$ACCOUNT/$REGION
 echo .
 echo .
 
