a94d891d0fd0b3b2dcb2d30f2e0b9b1361a52c96
Kwonyul <kwonyul@amazon.com>
2022-06-20 22:54:36 +0900

    Update: modify load script file for ECSContainer

--- codes/load-tester-script/app/entrypoint.sh
+++ codes/load-tester-script/app/entrypoint.sh
@@ -3,17 +3,20 @@
 echo --TARGET-URL--
 export URL_ALB=http://$AlbDnsName/items
 export URL_NAMESPACE=http://$TargetServiceName.$Namespace/items
-echo $URL
+echo "URL_ALB>> $URL_ALB"
+echo "URL_NAMESPACE>> $URL_NAMESPACE"
 
-echo --RESPONSE-TEST--
-curl $URL_ALB
 
 function ab_function {
+    echo --ALB-RESPONSE-TEST--
+    curl -X GET $URL_ALB
     ab -n 50 -c 2 $URL_ALB
+
+    echo --NS-RESPONSE-TEST--
+    curl -X GET $URL_NAMESPACE
     ab -n 50 -c 2 $URL_NAMESPACE
 
 }
 
 echo --START-LOAD-TEST--
-while true; do ab_function; sleep 5; done
-# while true; do ab -n 50 -c 2 $URL; sleep 5; done
+while true; do ab_function; sleep 10; done
