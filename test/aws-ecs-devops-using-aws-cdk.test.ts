4e01b1fd3e1b727d8ccf63b4e65668413957c0f0
Kwonyul <kwonyul@amazon.com>
2022-06-22 14:27:58 +0900

    Fix: compile error in testcase

--- test/aws-ecs-devops-using-aws-cdk.test.ts
+++ test/aws-ecs-devops-using-aws-cdk.test.ts
@@ -1,11 +1,22 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as AwsEcsDevopsUsingAwsCdk from '../infra/stack/aws-ecs-devops-using-aws-cdk-stack';
+import * as target from '../infra/ecs-service/ecs-common-service-stack';
+import { AppContext } from '../lib/template/app-context';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const appContext = new AppContext({
+        appConfigFileKey: 'APP_CONFIG',
+    });
+
     // WHEN
-    const stack = new AwsEcsDevopsUsingAwsCdk.AwsEcsDevopsUsingAwsCdkStack(app, 'MyTestStack');
+    const stack = new target.EcsCommonServiceStack(appContext, {
+        "Name": "LoadTesterScriptStack",
+
+        "TargetStack": "SampleBackendFastapiStack",
+
+        "AppPath": "codes/load-tester-script",
+        "DesiredTasks": 1
+    });
+
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
